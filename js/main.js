7c0b1c6e509dd7596affb67581d3889ab60aa587
Tony Hsieh <cstony0917@gmail.com>
2013-09-26 02:09:28 +0800

    slider

--- js/main.js
+++ js/main.js
@@ -39,13 +39,23 @@ function nano(template, data) {
 var ib = 'https://raw.github.com/cstony0917/warofpic/gh-pages/storage/';
 var span = [2,3,4,6];
 $(function(){
+  var i = 0;
   $.getJSON('db.json',function(r){
       $.each(r, function(k,v) {
-        console.log(k);
+        var active = null;
+        if(i ==0 ){
+          active = 'active';
+          $('#cat .carousel-indicators').append('<li data-target="#cat" class="active" data-slide-to="' + i +'"></li>');
+        }else{
+          active = null;
+          $('#cat .carousel-indicators').append('<li data-target="#cat" data-slide-to="' + i +'"></li>');
+        }
         var data = {
           name:k,
+          active :active
         };
-        $('#cat').append(nano($('#tmp .galleryTMP').html(),data));
+        $('#cat .carousel-inner').append(nano($('#tmp .galleryTMP').html(),data));
+
         // var data = {
         //   name : k,
         //   src
@@ -53,14 +63,17 @@ $(function(){
         $.each(this, function(name, value) {
             var src = ib + k + '/'+ value;
             var r = $.shuffle(span);
-            r = r[0]
-
+            r = r[0];
             var data = {
               src : src,
               r:r
             };
-            $('div.' + k + ' ul').append(nano($('#tmp .imageList').html(),data));
+            var ul  = $('div.' + k + ' ul');
+            ul.append(nano($('#tmp .imageList').html(),data));
+
         });
+        i++;
       });
+      $('.carousel').carousel()
   });
 });
