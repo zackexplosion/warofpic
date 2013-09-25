b977cc94031eef1e02a76b6e2edfd66f78ee135e
Tony Hsieh <cstony0917@gmail.com>
2013-09-26 00:32:08 +0800

    basic list

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-var ib = 'https://raw.github.com/cstony0917/warofpic/gh-pages/storage/'
+//image base
+var ib = 'https://raw.github.com/cstony0917/warofpic/gh-pages/storage/';
+
 $(function(){
   $.getJSON('db.json',function(r){
       $.each(r, function(k,v) {
         console.log(k);
-        $('#cat').append('<li class="' + k +'"><a href="">' + k + '</a></li>');
+        $('#cat').append('<li class="' + k +'"><a href="">' + k + '</a><ul></ul></li>');
         $.each(this, function(name, value) {
-          /// do stuff
-          console.log(name + '=' + value);
+            var src = ib + k + '/'+ value;
+            $('li.' + k + ' ul').append('<li><img src="' +  src + '" /></li>');
         });
       });
   });
