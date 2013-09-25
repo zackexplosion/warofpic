1277b2cb4a4b7357728f14c2ccdc7d2d02b6a72e
Tony Hsieh <cstony0917@gmail.com>
2013-09-26 00:25:49 +0800

    database maker

---
+++ dbmaker.php
@@ -0,0 +1,24 @@
+<?php
+//run on ever time image changes
+$root = 'storage';
+$files = scandir($root);
+unset($files[0],$files[1]);
+
+
+$pic = array();
+foreach ($files as $key => $value) {
+	$dir = $root . '/' . $value;
+	if(is_dir($dir)){
+		$images = scandir($dir);
+		unset($images[0],$images[1]);
+
+		foreach ($images as $k => $v) {
+			$pic[$value][] = $v;
+		}		
+	}	
+}
+
+file_put_contents('db.json', json_encode($pic));
+
+
+?>
